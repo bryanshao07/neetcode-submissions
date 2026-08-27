d24a8b4886fe45fcd5c995db839bd09520ffbccb
Bryan Shao <bs887@cornell.edu>
2026-08-27 00:02:17 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        match = {
+            "}" : "{",
+            ")" : "(",
+            "]" : "["
+        }
+        stack = []
+        for char in s:
+            if char not in match:
+                stack.append(char)
+            else:
+                if not stack or stack[-1] != match[char]:
+                    return False
+                stack.pop()
+        return not stack
